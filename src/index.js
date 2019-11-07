d57b46f4c2a1438e140f6afcdc34acc953a8a58c
DeAndre Johnson <dre@phreshr.com>
2019-11-07 08:05:11 +0800

    create store and wrap app in provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { reducer, initialState } from './reducers';
+
+const store = createStore(reducer, initialState);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement,
+);
